11a5f61405954f3136b91cfa38f5a832e2968db4
KaifKol <komrad02kb2020@gmail.com>
2024-12-28 16:31:36 +0200

    fix bugs

--- commands/ping.py
+++ commands/ping.py
@@ -7,7 +7,7 @@ class PingCommand(commands.Cog):
 
     @commands.slash_command(name="ping", description="узнать мою задержку.. o((>ω< ))o")
     async def ping(self, inter: disnake.ApplicationCommandInteraction):
-        await inter.response.send_message(f"Задержка: {round(self.bot.latency * 1000)}мс （＞人＜；）", ephemeral=True)
+        await inter.response.send_message(f"Задержка: {round(self.bot.latency * 1000)} мс （＞人＜；）", ephemeral=True)
 
 def setup(bot: commands.Bot):
     bot.add_cog(PingCommand(bot))
